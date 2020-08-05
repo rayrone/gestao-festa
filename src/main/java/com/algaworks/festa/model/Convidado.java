d4d1c9dc911a538c1fb2adb7fa7bdc7da6bc577c
Rayrone Zirtany <rayronezirtany@gmail.com>
2020-08-05 17:28:40 -0300

    Configuração do projeto para persistir os dados no banco PostgreSQL
    - Adição de dependência do driver do PostgreSQL no pom.xml
    - Alteração da classe Convidado para persistir no banco PostgreSQL usando uma sequence

--- src/main/java/com/algaworks/festa/model/Convidado.java
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -1,8 +1,6 @@
 package com.algaworks.festa.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -14,7 +12,11 @@ public class Convidado implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    // H2
+    //@GeneratedValue(strategy = IDENTITY)
+    // PostgreSQL
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "convidado_generator")
+    @SequenceGenerator(name="convidado_generator", sequenceName = "convidado_seq", initialValue = 4)
     private Long id;
 
     private String nome;
